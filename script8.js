f2b9e0228bee0a7fe3db304b928fb720c1a581a7
Landon B <125074736+countdown24@users.noreply.github.com>
2024-02-22 12:21:28 -0500

    Update script8.js

--- script8.js
+++ script8.js
@@ -1,11 +1,11 @@
 var clicks = 0;
 var refreshRate = 100; //time in milliseconds
-var currentYearPrefix = 24;
+var currentYearPrefix = 25;
 
 function updateTime() {
   let currentTime = new Date();
 
-  const christmasDate = new Date('2/14/20' + currentYearPrefix) //2021
+  const christmasDate = new Date('2/14/21' + currentYearPrefix) //2021
 
   let milliseconds;
   let days;
